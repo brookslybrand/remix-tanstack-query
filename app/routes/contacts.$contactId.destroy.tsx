174bd0a77b35b99d7fedb647d9769f69572b8da9
Brooks Lybrand <brookslybrand@gmail.com>
2024-07-04 15:43:29 -0500

    Implement destroy action

--- app/routes/contacts.$contactId.destroy.tsx
+++ app/routes/contacts.$contactId.destroy.tsx
@@ -1,11 +1,25 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
 import { deleteContact } from "../data";
+import { contactDetailQuery, queryClient } from "~/utils/query.client";
+import { ClientActionFunctionArgs, redirect } from "@remix-run/react";
 
 export const action = async ({ params }: ActionFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
   await deleteContact(params.contactId);
+  return {};
+};
+
+export const clientAction = async ({
+  serverAction,
+  params,
+}: ClientActionFunctionArgs) => {
+  const { contactId } = params;
+  invariant(contactId, "Missing contactId param");
+  // TODO: Implement the error handling
+  await serverAction();
+  const query = contactDetailQuery(contactId);
+  queryClient.removeQueries({ queryKey: query.queryKey });
   return redirect("/");
 };
